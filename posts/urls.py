69014efa9cf401b4364c1c69b7fa74e12c12e27b
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-18 22:21:44 -0400

    list_post and post_detail api views

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('homepage/', views.homepage, name='posts_home'),
+    path('', views.list_posts, name='list_posts'),
+    path('<int:post_index>', views.post_detail, name='post_detail'),
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,26 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+posts=[
+
+    {
+        'id':1,
+        'title':'title1',
+        'content':'content1'
+    },
+     {
+        'id':2,
+        'title':'title2',
+        'content':'content2'
+    },
+    {
+        'id':3,
+        'title':'title3',
+        'content':'content3'
+    }
+
+]
+
 @api_view(http_method_names=["GET", "POST"])
 def homepage(request:Request):
 
@@ -13,6 +33,47 @@ def homepage(request:Request):
 
     response={'message':'Hello World'}
     return Response(data=response, status=status.HTTP_200_OK)
+
+@api_view(http_method_names=['GET'])
+def list_posts(request:Request):
+    return Response(data=posts, status=status.HTTP_200_OK)
+
+@api_view(http_method_names=['GET'])
+def post_detail(request:Request, post_index:int):
+    post = posts[post_index]
+
+    if post:
+        return Response(data=post, status=status.HTTP_200_OK)
+
+    return Response(data={"error":"Post not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from rest_framework.request import Request
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+
+# @api_view(http_method_names=["GET", "POST"])
+# def homepage(request:Request):
+
+#     if request.method == "POST":
+#         data = request.data
+#         response={'message':'Hello World', 'data':data}
+#         return Response(data=response, status=status.HTTP_201_CREATED)
+
+#     response={'message':'Hello World'}
+#     return Response(data=response, status=status.HTTP_200_OK)
 
 
 
