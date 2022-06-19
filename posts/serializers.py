1ac522eccfd4b20306c7e12364ada6acd83e62b4
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-19 13:42:26 -0400

    testing max lenght for post title

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,9 +2,26 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=50)
     class Meta:
         model=Post 
         fields=['id', 'title', 'content', 'created']
+
+
+
+
+
+
+
+
+
+# from rest_framework import serializers 
+# from .models import Post
+
+# class PostSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=Post 
+#         fields=['id', 'title', 'content', 'created']
 
 
 
